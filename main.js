4f5836ef2ddce90eef1a507d75fbfb280b85f97e
viih00 <108474633+viih00@users.noreply.github.com>
2023-09-28 11:30:09 -0300

    Update main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+function toca Som(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+       elemento.play(); 
+    }
+    else{
+        //alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+}
