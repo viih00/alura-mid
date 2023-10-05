c7626dbcd906406514547fd0925f2cc5849de175
viih00 <108474633+viih00@users.noreply.github.com>
2023-10-05 10:58:11 -0300

    Update main.js

--- main.js
+++ main.js
@@ -9,3 +9,15 @@ function toca Som(seletorAudio) {
         console.log('Elemento não encontrado ou seletor inválido');
     }
 }
+
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+}
+
